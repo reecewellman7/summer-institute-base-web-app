3f782e860a75d77f3780ccb254ae873f06ceea8d
Reece Wellman <reecewellman26@lakewoodlocal.k12.oh.us>
2024-06-21 09:16:30 -0400

    complete blender project

--- app.rb
+++ app.rb
@@ -18,24 +18,113 @@ class App < Sinatra::Base
   def title
     'Very good app, not a virus'
   end
-
-  get '/examples' do
-    erb(:examples)
+  
+  def accounts 
+      Process.groups.map do |gid|
+        Etc.getgrgid(gid).name
+      end.select do |group|
+          group.start_with?('P')
   end
+ end
+ def blend_files
+     Dir.glob("#{__dir__}/blend_files/*.blend")
+ end
+ 
+ def images(directory) 
+    Dir.glob("#{directory}/*.png") 
+ end
+ 
+ post '/render/frames' do
+     
+     walltime = format('%02d:00:00', params[:walltime])
+     blend_file = "#{params[:blend_file]}'"
+     
+     args = ['-A', params[:account], '-n', params[:num_cpus]]
+     args.concat(['-t', walltime, '-M', 'pitzer', '--parsable'])
+     args.concat(['--export',"BLEND_FILE_PATH=#{params[:blend_file]},OUTPUT_DIR=#{params[:project_directory]},FRAME_RANGE=#{params[:frame_range]}"])
+     
+     script = "#{__dir__}/scripts/render_frames.sh"
+     
+    output = `/bin/sbatch #{args.join(' ')} #{script} 2>&1`
+     
+     
+     
+    session[:flash] = { info: "Submitted job with output '#{output}'"}
+    
+    redirect(url("/projects/#{File.basename(params[:project_directory])}"))
+ end
+  
+  
+  post '/render/video' do
+      
+      walltime = format('%02d:00:00', params[:walltime])
+      script = "#{__dir__}/scripts/render_video.sh"
+      exports= "FRAMES_PER_SEC=#{params[:frames_per_second]}"
+      exports= "#{exports},OUTPUT_DIR=#{params[:project_directory]}"
+      
+      args = ['-A', params[:account], '-n', params[:num_cpus]]
+      args.concat(['-M', 'pitzer', '--parsable'])
+      args.concat(['-t', walltime, '--export', exports])
+      
+      
+      
+    output = `/bin/sbatch #{args.join(' ')} #{script} 2>&1`
+      
+      
+      session[:flash] = {info: "submitting job with output: #{output}" }
+      redirect(url("/projects/#{File.basename(params[:project_directory])}"))
+  end
+      
+  def sanitize_project_name(name)
+      name.downcase.gsub(' ','_')
+  end
+  
+  def projects_root
+     "#{__dir__}/projects" 
+  end
+  def projects
+      Dir.children(projects_root).select do |path|
+          Pathname.new("#{projects_root}/#{path}").directory?
+    end.sort_by(&:to_s)
+end 
+
+
+    get '/examples' do
+        erb(:examples)
+    end
 
   get '/' do
     logger.info('requsting the index')
     @flash = session.delete(:flash) || { info: 'Welcome to Summer Institute!' }
     erb(:index)
   end
-  
-  get '/projects/new' do
+  get '/projects/:name' do
+      if params[:name] == 'new'
       erb(:new_project)
-  end
-  
-  
+  else 
+      
+      @directory = Pathname.new("#{projects_root}/#{params[:name]}")
+      @flash = session.delete(:flash)
+      @images = images(@directory)
+      
+      
+    if(@directory.exist? && @directory.directory?)
+      
+      erb(:show_project)
+    else
+        session[:flash] = { danger: "There is no project named '#{params[:name]}'."}
+      
+      redirect(url("/"))
+    end
+  end 
+ end 
   post '/projects/new' do
-      redirect(url("/projects/new"))
+      directory_name = sanitize_project_name params[:name] 
+      
+      FileUtils.mkdir_p("#{projects_root}/#{directory_name}")
+      session[:flash] = { info: "Created project '#{params[:name]}'."}
+      
+      redirect(url("/"))
   end
 end
 
--- scripts/render_video.sh
+++ scripts/render_video.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+cd $OUTPUT_DIR
 module load ffmpeg/4.0.2
-ffmpeg -r "$FRAMES_PER_SEC" -y -i "$OUTPUT_DIR/render_%04d.png" -vsync vfr -b:v 16M -pix_fmt yuv420p "$OUTPUT_DIR/video.mp4"
+ffmpeg -r "$FRAMES_PER_SEC" -y -pattern_type glob -i "*.png" -vsync vfr -b:v 16M -pix_fmt yuv420p "video.mp4"
